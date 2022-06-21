7064d46dba7bc39a6067f55dfa31ff12dd98b37a
Isaac Mbira <inm206@nyu.edu>
2022-06-21 20:49:14 +1000

    Added logic to prevent overlap when adding multiple shapes

--- src/components/FabricCanvas.vue
+++ src/components/FabricCanvas.vue
@@ -1,5 +1,7 @@
 <template>
-  <canvas ref="canvas" width="400" height="400" class="border"></canvas>
+  <div class="justify-center flex">
+    <canvas ref="canvas" width="600" height="600" class="border"></canvas>
+  </div>
   <br><br>
   <div class="container mx-auto">
     <CanvasControls @add-square="addSquare" @add-circle="addCircle" @remove="removeObject" @send-to-back="sendToBack" @bring-to-front="bringToFront" @export="exportToPng" @undo="undo" @redo="redo" />
@@ -20,6 +22,13 @@ export default {
     CanvasControls
   },
 
+  data () {
+    return {
+      intSquareCount: 0,
+      intCircleCount: 0
+    }
+  },
+
   mounted() {
     const ref = this.$refs.canvas;
     canvas = new fabric.Canvas(ref);
@@ -34,19 +43,31 @@ export default {
   methods: {
     addSquare() {
       const square = new fabric.Rect({
+        left: this.intSquareCount * 20,
+        top: this.intSquareCount * 20,
         fill: 'red',
         width: 20,
         height: 20
       });
       canvas.add(square);
+      this.intSquareCount++
+      if (this.intSquareCount == 30) {
+        this.intSquareCount = 0
+      }
     },
 
     addCircle() {
       const circle = new fabric.Circle({
+        left: this.intCircleCount * 45,
+        top: this.intCircleCount * 45,
         fill: 'blue',
         radius: 30
       });
       canvas.add(circle);
+      this.intCircleCount++
+      if (this.intCircleCount == 13) {
+        this.intCircleCount = 0
+      }
     },
 
     removeObject() {
